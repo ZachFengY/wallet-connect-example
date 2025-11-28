eb61f7ae510af46c46fa547ddbd6567581e97dac
zachzach <zach@tomo.inc>
2025-11-28 10:57:59 +0800

    fix demo in mobile

--- dist/assets/index-BAq7SrDa.js
+++
@@ -1,1 +0,0 @@
-import{J as a}from"./features-animation-CHI9iLKD.js";import"./index-Clbi6dGC.js";var i=a;export{i as default};
---
+++ dist/assets/src-UW24ZMRV-DV5sXvYX.js
@@ -0,0 +1,1 @@
+import{J as a}from"./features-animation-CGGVF-sd.js";import"./index-BMHd9UbT.js";var t=a;export{t as default};
