b746ee909731b26f7dc08a013423aae6b4b56107
zachzach <zach@tomo.inc>
2025-12-19 15:55:01 +0800

    update demo

--- dist/assets/src-UW24ZMRV-BMJz1kn6-C8jp252m.js
+++
@@ -1,1 +0,0 @@
-import{K as r}from"./features-animation-DkkYMPJT-WwKq_gWj.js";import"./index-D5LbuyCQ.js";var t=r;export{t as default};
---
+++ dist/assets/src-UW24ZMRV-k-Z_HMsP-BbNlWp4G.js
@@ -0,0 +1,1 @@
+import{K as r}from"./features-animation-BxG5gMsu-DzVDLppa.js";import"./index-CmM8-1ux.js";var t=r;export{t as default};
