3e6260e2f4170faf029ddcaccac8216c122db68c
zachzach <zach@tomo.inc>
2025-12-19 15:58:51 +0800

    update config cs

--- dist/assets/src-UW24ZMRV-k-Z_HMsP-BbNlWp4G.js
+++
@@ -1,1 +0,0 @@
-import{K as r}from"./features-animation-BxG5gMsu-DzVDLppa.js";import"./index-CmM8-1ux.js";var t=r;export{t as default};
---
+++ dist/assets/src-UW24ZMRV-k-Z_HMsP-D8S-x7EB.js
@@ -0,0 +1,1 @@
+import{K as r}from"./features-animation-BxG5gMsu-S7x__u9k.js";import"./index-bBjx92tI.js";var t=r;export{t as default};
